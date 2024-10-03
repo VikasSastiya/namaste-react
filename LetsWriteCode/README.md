6d7e2daa4fa75f193a713b88ae34e4726a7d144f
VikasSastiya <167347952+VikasSastiya@users.noreply.github.com>
2024-10-03 18:25:46 +0530

    Part

--- LetsWriteCode/README.md
+++ LetsWriteCode/README.md
@@ -1,1 +1,8 @@
-fhfg
+![alt text](image.png)
+![alt text](image-1.png)
+![alt text](image-2.png)
+
+# use the "ctrl+`" to open terminal
+
+-  use node app.js to run the command in terminal
+![alt text](image-3.png)
---
+++ LetsWriteCode/app.js
@@ -0,0 +1,15 @@
+var name="Namaste NodeJS";
+
+var a=10;
+
+var b=20;
+
+console.log(name);
+console.log(a+b);
+
+
+// if you type "window" in console in browser you find a big object, to find global object in node type below syntex
+// console.log(global);
+// console.log(this);   // empty object
+// console.log(globalThis);
+console.log(globalThis==global)
